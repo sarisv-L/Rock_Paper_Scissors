b24550755b757053e3f113e66faa2529069bcf39
sarisv-L <sarisv@gmx.de>
2024-08-14 21:16:15 +0200

    Finished RockPaperScissors Game running on console

--- script.js
+++ script.js
@@ -18,4 +18,45 @@ const getHumanChoice = function () {
   }
 };
 
-console.log(getComputerChoice(), getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+const playRound = function (humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a Tie, no one gets a point!");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1;
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+    humanScore += 1;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+    humanScore += 1;
+  } else {
+    console.log(`You Lost! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+};
+
+const determineWinner = function () {
+  console.log("Final Scores:");
+  console.log(`Human: ${humanScore}`);
+  console.log(`Computer: ${computerScore}`);
+
+  if (humanScore > computerScore) {
+    console.log("Congratulations! You won!");
+  } else if (computerScore > humanScore) {
+    console.log("The computer wins this time. Better luck next time!");
+  }
+};
+
+const playGame = function () {
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+  determineWinner();
+};
+
+playGame();
